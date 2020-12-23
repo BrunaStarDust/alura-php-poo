e513abbc2adeb10972558c1d0e4dc437f088b88f
Bruna <bruna0chan@gmail.com>
2020-12-23 09:30:50 -0300

    building and testing class Conta

--- index.php
+++ index.php
@@ -9,7 +9,19 @@
     <pre>
         <?php
         require_once 'src/Conta.php';
-        $c1 = new Conta();
+        $c1 = new Conta('999.999.999-99', 'Moquidésia');
+        $c2 = new Conta('777.777.777-77', 'Irineu');
+        print_r($c1);
+        print_r($c2);
+        $c1->deposita(2000);
+        print_r($c1);
+        $c1->saca(496.75);
+        print_r($c1);
+        $c1->deposita(200);
+        print_r($c1);
+        $c1->transfere(700, $c2);
+        print_r($c1);
+        print_r($c2);
         ?>
     </pre>
 </body>
--- src/Conta.php
+++ src/Conta.php
@@ -6,4 +6,72 @@ class Conta
     private $cpfTitular;
     private $nomeTitular;
     private $saldo;
+
+    public function __construct($cpfTitular, $nomeTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+        $this->nomeTitular = $nomeTitular;
+        $this->saldo = 0;
+    }
+
+    public function saca($valorSacado)
+    {
+        if ($valorSacado > $this->getSaldo()) {
+            echo "<p>Você não pode sacar</p>";
+        } else {
+            $this->setSaldo($this->getSaldo() - $valorSacado);
+            echo "<p>Saque realizado com sucesso!</p>";
+        }
+    }
+
+    public function deposita($valorDepositado)
+    {
+        if ($valorDepositado < 0) {
+            echo "<p>Você só pode depositar um valor positivo</p>";
+        } else {
+            $this->setSaldo($this->getSaldo() + $valorDepositado);
+            echo "<p>Depósito realizado com sucesso!</p>";
+        }
+    }
+
+    public function transfere($valorTransferido, $contaDestino)
+    {
+        if ($valorTransferido > $this->getSaldo()) {
+            echo "Saldo indisponível";
+        } else {
+            $this->saca($valorTransferido);
+            $contaDestino->deposita($valorTransferido);
+            echo "<p>Transferência realizada com sucesso!</p>";
+        }
+    }
+
+    public function getCpfTitular()
+    {
+        return $this->cpfTitular;
+    }
+
+    public function setCpfTitular($cpfTitular)
+    {
+        $this->cpfTitular = $cpfTitular;
+    }
+
+    public function getNomeTitular()
+    {
+        return $this->nomeTitular;
+    }
+
+    public function setNomeTitular($nomeTitular)
+    {
+        $this->nomeTitular = $nomeTitular;
+    }
+
+    public function getSaldo()
+    {
+        return $this->saldo;
+    }
+
+    public function setSaldo($saldo)
+    {
+        $this->saldo = $saldo;
+    }
 }
